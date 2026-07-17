d34fb3b5c371fb7fa58abe36bd1efd8b918eb6f5
kavyaalla04 <kavya.alla@prodapt.com>
2026-07-17 17:45:29 +1300

    example1.js

---
+++ Day10/example1.js
@@ -0,0 +1,11 @@
+let name = "abc"
+const age = 30
+var location = "Chennai"
+console.log(location)
+
+//let name = "xyz" //redeclaration of let is not allowed
+var location = "Bangalore"
+
+console.log(name)
+console.log(age)
+console.log(location)
